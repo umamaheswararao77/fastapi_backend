addae2f787836cb69bfd662643c7c126ee28fdb2
uma <umamaheswararaovippadapu@gmail.com>
2026-02-06 14:47:35 +0530

    Use DATABASE_URL from environment for production

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://user1:user123@localhost:5432/userdb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
